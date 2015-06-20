9e10d70c19fa26d02b0ea918e13ff09544e83929
NICOLAS <NICOLAS@NICOLAS-PC>
2015-06-20 20:51:55 -0300

    versao 1.0

---
+++ src/engsoftware/A.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package engsoftware;
+
+/**
+ *
+ * @author NICOLAS
+ */
+public class A {
+
+    private int A1;
+
+    public int getA1() {
+        return A1;
+    }
+
+    public void setA1(int A1) {
+        this.A1 = A1;
+    }
+
+    private float A2;
+
+    public float getA2() {
+        return A2;
+    }
+
+    public void setA2(float A2) {
+        this.A2 = A2;
+    }
+
+    public  void main(String[] args) {
+        
+    }
+    
+}
